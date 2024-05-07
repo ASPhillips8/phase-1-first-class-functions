ec22fc19d63ea77686e927e0200cf4171ac8d985
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-07 10:32:18 -0400

    Create first_class functions

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback) {
+  callback ();
+}
+
+function  returnsANamedFunction() {
+  return function guitarName(){
+    console.log (`This is a Gibson Les Paul.`);
+  }
+}
+function returnsAnAnonymousFunction() {
+  return function () {
+    console.log (`This is an SG.`);
+  }
+}
+
